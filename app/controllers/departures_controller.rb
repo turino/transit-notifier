29a5da5a155acfc2e2b6d29bc7ea6b4807bc1e76
Jacob Turino <jacob@thoughtbot.com>
2017-12-08 15:33:58 -0800

    Temp departures code for testing pi

--- app/controllers/departures_controller.rb
+++ app/controllers/departures_controller.rb
@@ -1,7 +1,19 @@
 class DeparturesController < ApplicationController
   def index
     text = "when #{params[:keyword]}"
-
-    render json: { estimates: TrainReporter.departures(text) }
+    departures = TrainReporter.departures(text)
+    next_train = departures.detect { |etd| etd > 4 }
+    color = case next_train
+            when 9..11
+              1
+            when 6..8
+              2
+            when 5
+              3
+            else
+              0
+            end
+    render json: color
+    # render json: { estimates: TrainReporter.departures(text) }
   end
 end
